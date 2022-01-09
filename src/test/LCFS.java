680733ecd4230c5b5dbf552054a23bae4dc4d441
dekrom26 <94851054+dekrom26@users.noreply.github.com>
2022-01-09 10:42:19 +0200

    Update LCFS.java

--- src/test/LCFS.java
+++ src/test/LCFS.java
@@ -1,9 +1,42 @@
 package test;
+import java.util.*;
 
 public class LCFS implements scheduling{
 
+    Stack<Process> stack,stack2;
+    int runtime;
+
     @Override
     public float calculate(Process[] pro) {
-        return 0;
+        for (int i=pro.length-1;i>0;i--)
+        {
+            runtime+=pro[i].bursTime;
+            stack.push(pro[i]);
+        }
+        int counter=1;
+        Process working =new Process(0,0);
+        working.clone(stack.pop());
+        int endwork=counter+working.bursTime;
+        ArrayList<Integer> TAT=new ArrayList<Integer>();
+        int prosTAT=0;
+        while (!stack.isEmpty())
+        {
+            if (counter==stack.peek().arriValTime){
+                stack2.push(stack.pop());
+            }
+            if (endwork<counter)   {
+                prosTAT=working.bursTime+(counter-working.arriValTime);//bursttime+waitingtime
+                TAT.add(prosTAT);
+                working.clone(stack2.pop());
+                endwork=counter+working.bursTime;
+            }
+            counter++;
+        }
+        float avgTAT=0;
+        for (Integer ta:TAT){
+            avgTAT+=(float)ta;
+        }
+        avgTAT=avgTAT/(float)TAT.size();
+        return avgTAT;
     }
 }
